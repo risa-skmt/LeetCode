395d9b65a33241d70b9851d1a180fb3d49b668d7
risa-skmt <skmt.risa@gmail.com>
2023-07-13 20:48:44 +0900

    time over

---
+++ JavaScript/049.js
@@ -0,0 +1,30 @@
+var groupAnagrams = function(strs) {
+    let newStrs = [];
+    strs.map(a => {
+        newStrs.push(a.split('').sort());
+    })
+    
+   let answer = [];
+   let index = [];
+   let A = null;
+   for(let i=0; i<=newStrs.length-1; i++){
+       if(index.includes(i)){
+           continue;
+       }
+       let pair = [];
+       A = newStrs[i].join();
+       index.push(i);
+       pair.push(strs[i]);
+
+       for(let k=i+1; k<=newStrs.length-1; k++){
+           if(newStrs[k].join() === A){
+               pair.push(strs[k]);
+               index.push(k);
+           }
+       }
+
+       answer.push(pair);
+       pair = [];
+   }
+    return answer;
+};
